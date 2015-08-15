90e48b47c42869a394dad643f033268c11c1e4b5
Evil-Erthanu <jeremydog1@gmail.com>
2015-08-15 19:07:51 +1200

    All the files
    Adding the actual stuff would be a good start

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clockCycle = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelPaddleY = new System.Windows.Forms.Label();
+            this.labelBallX = new System.Windows.Forms.Label();
+            this.labelBallY = new System.Windows.Forms.Label();
+            this.labelYV = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelXV = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(13, 34);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(843, 531);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(868, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 577);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Paddle Y = ";
+            // 
+            // clockCycle
+            // 
+            this.clockCycle.Enabled = true;
+            this.clockCycle.Interval = 33;
+            this.clockCycle.Tick += new System.EventHandler(this.clockCycle_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 577);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ball X = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 577);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ball Y = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(504, 577);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "yV = ";
+            // 
+            // labelPaddleY
+            // 
+            this.labelPaddleY.AutoSize = true;
+            this.labelPaddleY.Location = new System.Drawing.Point(185, 577);
+            this.labelPaddleY.Name = "labelPaddleY";
+            this.labelPaddleY.Size = new System.Drawing.Size(35, 13);
+            this.labelPaddleY.TabIndex = 6;
+            this.labelPaddleY.Text = "label5";
+            // 
+            // labelBallX
+            // 
+            this.labelBallX.AutoSize = true;
+            this.labelBallX.Location = new System.Drawing.Point(281, 577);
+            this.labelBallX.Name = "labelBallX";
+            this.labelBallX.Size = new System.Drawing.Size(35, 13);
+            this.labelBallX.TabIndex = 7;
+            this.labelBallX.Text = "label5";
+            // 
+            // labelBallY
+            // 
+            this.labelBallY.AutoSize = true;
+            this.labelBallY.Location = new System.Drawing.Point(376, 577);
+            this.labelBallY.Name = "labelBallY";
+            this.labelBallY.Size = new System.Drawing.Size(35, 13);
+            this.labelBallY.TabIndex = 8;
+            this.labelBallY.Text = "label5";
+            // 
+            // labelYV
+            // 
+            this.labelYV.AutoSize = true;
+            this.labelYV.Location = new System.Drawing.Point(533, 577);
+            this.labelYV.Name = "labelYV";
+            this.labelYV.Size = new System.Drawing.Size(35, 13);
+            this.labelYV.TabIndex = 9;
+            this.labelYV.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 577);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "xV = ";
+            // 
+            // labelXV
+            // 
+            this.labelXV.AutoSize = true;
+            this.labelXV.Location = new System.Drawing.Point(456, 577);
+            this.labelXV.Name = "labelXV";
+            this.labelXV.Size = new System.Drawing.Size(35, 13);
+            this.labelXV.TabIndex = 11;
+            this.labelXV.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 599);
+            this.Controls.Add(this.labelXV);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelYV);
+            this.Controls.Add(this.labelBallY);
+            this.Controls.Add(this.labelBallX);
+            this.Controls.Add(this.labelPaddleY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer clockCycle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelPaddleY;
+        private System.Windows.Forms.Label labelBallX;
+        private System.Windows.Forms.Label labelBallY;
+        private System.Windows.Forms.Label labelYV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelXV;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        bool gameStarted = false;
+
+        //ball starting position
+        int ballX = 0;
+        int ballY = 0;
+
+        int bottomLimit = 0;
+        int rightLimit = 0;
+        int centerX = 0;
+
+        //Various Paddle related variables
+        
+        //the starting Y value of the paddle
+        int paddlePosition = 0;
+        //the paddle's starting x pos
+        int paddleX = 50;
+
+        int AIPaddleX = 0;
+        int AIPaddlePosition = 0;
+
+        const int PADDLE_LENGTH = 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //get the center of the game Board
+            ballX = pictureBoxDisplay.Width / 2;
+            ballY = pictureBoxDisplay.Height / 2;
+
+            bottomLimit = pictureBoxDisplay.Height;
+            rightLimit = pictureBoxDisplay.Width;
+            centerX = pictureBoxDisplay.Width / 2;
+
+            //get the starting X position of the AI paddle
+            AIPaddleX = pictureBoxDisplay.Width - 50;
+
+            //set the starting point for the player's paddle
+            paddlePosition = centerX - (PADDLE_LENGTH /2);
+
+        }
+
+        /// <summary>
+        /// Draw up some shizzle to make it look pretty at the start or something
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        /// <summary>
+        /// set the board back to the original state
+        /// </summary>
+        private void NewGame()
+        {
+            gameStarted = false;
+
+            ballX = pictureBoxDisplay.Width / 2;
+            ballY = pictureBoxDisplay.Height / 2;
+
+            xV = -10;
+            yV = 0;
+
+            pictureBoxDisplay.Refresh();
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Pen pen1 = new Pen(Color.White, 10);
+            Pen dashPen = new Pen(Color.White, 3);
+            dashPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+
+            DrawBoard(paper, dashPen, pen1);
+            DrawABall(ballX, ballY);
+            DrawPaddle(paper, pen1);
+
+            //delete the no longer used graphics objects
+            pen1.Dispose();
+            dashPen.Dispose();
+            paper.Dispose();
+        }
+
+        /// <summary>
+        /// Close the Application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();  
+        }
+
+        #region Paddle Control
+
+        /// <summary>
+        /// If an arrow key is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            gameStarted = true;  
+            
+            if (e.KeyCode == Keys.Up && paddlePosition > 0)
+            {
+                paddlePosition -= 12;
+            }
+            else if (e.KeyCode == Keys.Down && paddlePosition < (pictureBoxDisplay.Height - PADDLE_LENGTH))
+            {
+                paddlePosition += 12;
+            }
+            label1.Text = "Paddle Y = " + paddlePosition.ToString();
+        }
+
+        private void DrawPaddle(Graphics paper, Pen pen1)
+        {
+            //draw the paddle
+            paper.DrawLine(pen1, paddleX, paddlePosition, paddleX, (paddlePosition + PADDLE_LENGTH));
+        }
+
+        #endregion
+
+        /// <summary>
+        /// On each tick of the clock, update the screen location
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clockCycle_Tick(object sender, EventArgs e)
+        {
+            if (gameStarted)
+            {
+                pictureBoxDisplay.Refresh();
+                Graphics paper = pictureBoxDisplay.CreateGraphics();
+                Pen pen1 = new Pen(Color.White, 10);
+                Pen dashPen = new Pen(Color.White, 3);
+                dashPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+
+                //draw the board
+                DrawBoard(paper, dashPen, pen1);
+
+                //draw the player paddle
+                DrawPaddle(paper, pen1);
+
+                //move the ball
+                MoveBall();
+
+                //move the AI Paddle
+                MoveAIPaddle();
+
+                //draw the AI Paddle
+                DrawAIPaddle(paper, pen1);
+
+                //draw the ball
+                DrawABall(ballX, ballY);
+
+                paper.Dispose();
+                pen1.Dispose();
+                dashPen.Dispose();
+            }
+        }
+
+        private void DrawBoard(Graphics paper, Pen dashPen, Pen pen1)
+        {
+            paper.DrawLine(dashPen, centerX, 0, centerX, bottomLimit);
+        }
+
+        #region Ball Control
+
+        Random rand = new Random();
+
+        //the ball's speed in each direction (pixels per "clock cycle"
+        int xV = -10;
+        int yV = 0;
+
+        /// <summary>
+        /// Draw the Ball
+        /// </summary>
+        /// <param name="ballX">the ball X position</param>
+        /// <param name="ballY">the ball Y position</param>
+        private void DrawABall(int ballX, int ballY)
+        {
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Pen pen1 = new Pen(Color.White, 5);
+            Rectangle ball = new Rectangle(ballX, ballY, 5, 5);
+
+            //draw the ball
+            paper.DrawRectangle(pen1, ball);
+            
+            //delete the drawing objects
+            paper.Dispose();
+            pen1.Dispose();
+        }
+
+        /// <summary>
+        /// Moves the ball around the screen and bounces it off walls
+        /// </summary>
+        private void MoveBall()
+        {
+            if (ColisionDetection())
+            {
+                Boop();
+
+                //come up with a random bounce direction
+
+                //if the ball is coming UP
+                if (yV <= 0)
+                {
+                    yV = rand.Next(-15, -5);
+                }
+
+                //if ball is going DOWN
+                if (yV > 0)
+                {                    
+                    yV = rand.Next(5, 15);
+                }
+                xV = rand.Next(15, 20);
+            }
+
+            //if the ball has hit the bottom limit
+            if (ballY > bottomLimit)
+            {
+                //bounce the ball
+                yV = ToOppositeSign(yV);
+            }
+
+            //if the ball has hit the right limit
+                //ie, it hits the computer paddle
+            else if (ballX > rightLimit - 50)
+            {
+                Boop();
+                //bounce the ball
+
+                //if the ball is coming up
+                //if the ball is coming UP
+                if (yV <= 0)
+                {
+                    yV = rand.Next(-15, -5);
+                }
+
+                //if ball is going DOWN
+                if (yV > 0)
+                {
+                    yV = rand.Next(5, 15);
+                }
+                xV = rand.Next(-20, -15);
+            }
+
+            //if the ball has hit the top of the board
+            else if (ballY < 0)
+            {
+                
+                //bounce the ball
+                yV = ToOppositeSign(yV);
+
+            }
+
+            //if the ball has hit the left of the board
+            else if (ballX < 0)
+            {
+                //bounce the ball
+                //xV = ToOppositeSign(xV);
+                NewGame();
+            }
+
+            //keep moving the ball as it is
+            ballY += yV;
+            ballX += xV;
+
+            //update the labels and stuff
+            labelBallX.Text = ballX.ToString();
+            labelBallY.Text = ballY.ToString();
+            labelYV.Text = yV.ToString();
+            labelXV.Text = xV.ToString();
+            labelPaddleY.Text = paddlePosition.ToString();
+        }
+
+        /// <summary>
+        /// Checks if the ball is in colision with the paddle
+        /// </summary>
+        /// <returns></returns>
+        private bool ColisionDetection()
+        {
+            if (ballX <= paddleX && ballY >= paddlePosition && ballY <= paddlePosition + PADDLE_LENGTH)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        /// <summary>
+        /// Change an int to it's opposite - or + value
+        /// this method is used to bounce the ball
+        /// </summary>
+        /// <param name="number">an int</param>
+        /// <returns>number's opposite - or + value</returns>
+        private int ToOppositeSign(int number)
+        {
+            //The ball has hit something, play a boop
+            Boop();
+
+            if (number > 0)
+            {
+                number = -number;
+                return number;
+            }
+            else if (number < 0)
+            {
+                number = number + (number * -2);
+                return number;
+            }
+            else return number;
+        }
+
+        /// <summary>
+        /// Play a boop
+        /// </summary>
+        private void Boop()
+        {          
+            {
+                //SoundPlayer simpleSound = new SoundPlayer(@"X:\C# Projects\Week 9\Pong\WindowsFormsApplication1\Resources\boop.wav");
+                //simpleSound.Play();
+            }
+        }
+
+        #endregion
+
+        #region AI
+
+        private void MoveAIPaddle()
+        {
+            //move the ai paddle
+            AIPaddlePosition = ballY - PADDLE_LENGTH / 2;
+        }
+
+        private void DrawAIPaddle(Graphics paper, Pen pen1)
+        {
+            //draw the paddle
+            paper.DrawLine(pen1, AIPaddleX, AIPaddlePosition, AIPaddleX, (AIPaddlePosition + PADDLE_LENGTH));
+
+            //delete the drawing objects
+            paper.Dispose();
+            pen1.Dispose();
+        }
+                
+        #endregion
+        }
+}
+
